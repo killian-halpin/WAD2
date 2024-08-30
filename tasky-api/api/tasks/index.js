eec971039f8b6c806763dc96428b7e5848316eec
Killian Halpin <you@example.com>
2024-08-30 20:20:48 +0100

    Upcoming Movies from TMDB.

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import Task from './taskModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
 // Get all tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find();
+    const tasks = await Task.find().populate('userId', 'username');
     res.status(200).json(tasks);
 });
 
 // create a task
-router.post('/', async (req, res) => {
+router.post('/', asyncHandler(async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
-});
-
+}))
 
 // Update Task
 router.put('/:id', async (req, res) => {
@@ -27,7 +27,7 @@ router.put('/:id', async (req, res) => {
     } else {
         res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
-})
+});
 
 // delete Task
 router.delete('/:id', async (req, res) => {
@@ -41,5 +41,4 @@ router.delete('/:id', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
 });
-
 export default router;
